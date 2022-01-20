57946e4c05d15e46443d7ce74f57cbd27abed9ae
Shahari <2053121@cognizant.com>
2022-01-20 15:57:33 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { Provider } from 'react-redux';
-import store from './store/index';
 require('typeface-inter')
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
